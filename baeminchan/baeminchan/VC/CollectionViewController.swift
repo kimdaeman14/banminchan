9d5d5fbd44ce3b04b785b7788a44156cd1285e40
kimdaeman14 <kimdaeman14@gmail.com>
2018-08-03 16:09:01 +0900

    배민찬연습 프로젝트 생성

---
+++ baeminchan/baeminchan/VC/CollectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewController.swift
+//  baeminchan
+//
+//  Created by kimdaeman14 on 2018. 8. 3..
+//  Copyright © 2018년 GoldenShoe. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+}
---
+++ baeminchan/baeminchan/VC/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  baeminchan
+//
+//  Created by kimdaeman14 on 2018. 8. 3..
+//  Copyright © 2018년 GoldenShoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction private func showCollectionViewController(_ sender: UIButton){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationViewController = storyboard.instantiateViewController(withIdentifier: "NavigationViewController")
+        navigationViewController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+        self.present(navigationViewController, animated: true)
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
