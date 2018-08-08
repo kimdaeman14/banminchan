25c120060f0f085674dbbae4ca46a1a4fe57c4fb
kimdaeman14 <kimdaeman14@gmail.com>
2018-08-08 14:07:00 +0900

    재시작 커스텀셀 테스트

--- collectiontest/collectiontest/ViewController.swift
+++ collectiontest/collectiontest/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let data = ["아침","점심","저녁"]
     
     @IBOutlet weak var tableView:UITableView!
 
@@ -20,9 +19,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        tableView.register(UINib(nibName: "CustomerServiceNib", bundle: nil), forCellWithReuseIdentifier: "CustomerServNib")
-        
-        tableView.register(UINib?, forCellReuseIdentifier: "CustomerServiceNib")
 
     }
 
@@ -36,29 +32,7 @@ extension ViewController : UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-//        let cell1 = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "CustomerServiceNib")
-        let cell1 = tableView.dequeueReusableCell(withIdentifier: "CustomerServiceNib", for: indexPath) as! CustomerServiceNib
-        
-        let cell2 = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "ProductInfoCell")
-        let cell3 = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "ProductInfoCell")
 
-    
-        switch data[indexPath.row] {
-        case "아침":
-            
-            return cell1
-        case "점심":
-            cell2.textLabel?.text = data[indexPath.row]
-
-            return cell2
-        case "저녁":
-            cell3.textLabel?.text = data[indexPath.row]
-
-            return cell3
-        default:
-            print("fail")
-        }
         
         return cell1
         
