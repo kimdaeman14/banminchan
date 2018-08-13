732bf910c7d116da66d9e828d7cc240bfae7f59f
kimdaeman14 <kimdaeman14@gmail.com>
2018-08-13 23:39:59 +0900

    딜리버리뷰셀추가

---
+++ collectiontest/collectiontest/New Group/DeliveryViewCell.swift
@@ -0,0 +1,25 @@
+
+//
+//  DeliveryViewCell.swift
+//  collectiontest
+//
+//  Created by kimdaeman14 on 2018. 8. 13..
+//  Copyright © 2018년 GoldenShoe. All rights reserved.
+//
+
+import UIKit
+
+class DeliveryViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
