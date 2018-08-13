b43c1d6e09adfb80400c7a184cca352b3e2f82fd
kimdaeman14 <kimdaeman14@gmail.com>
2018-08-13 16:28:42 +0900

    예제 완성

--- delegateTest/delegateTest/ViewController.swift
+++ delegateTest/delegateTest/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField:UITextField!
+    @IBOutlet weak var label:UILabel!
+    
+    @IBAction private func buttonTaped(_ sender: UIButton){
+//        label.text = textField.text
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        label.text = textField.text
+        return true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.delegate = self //텍스트필드에 뭔가변하면 내가(뷰컨트롤러) 알려줄게.
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
